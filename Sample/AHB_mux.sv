ec0c55dc51722c6de214a97409cdbb2a4b786e73
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2020-12-03 17:16:54 +0700

    Update AHB_mux.sv

--- Sample/AHB_mux.sv
+++ Sample/AHB_mux.sv
@@ -1,19 +1,38 @@
 //////////////////////////////////////////////////////////////////////////////////
-// File Name: 		AHB_mux.sv
+// File Name: 		AHB_mux#NUM#.sv
 // Project Name:	AHB_Gen
 // Email:         quanghungbk1999@gmail.com
 // Version    Date      Author      Description
-// v0.0       2/10/2020 Quang Hung  First Creation
+// v0.0       3/10/2020 Quang Hung  First Creation
 //////////////////////////////////////////////////////////////////////////////////
 
+//================================================================================
+//#CONFIG_GEN#
+//================================================================================
+
 import AHB_package::*;
-module AHB_bus
+module AHB_mux#NUM#
 #(
-  parameter AHB
+    parameter CHANNEL_NUM = 4,
+    `ifdef MAS
+    parameter PAY_LOAD = 78 
+    `elsif SLV
+    parameter PAY_LOAD = 33 
+    `endif
 )
 (
-
+    input  [CHANNEL_NUM-1:0][PAY_LOAD-1:0] payload_in,
+    input  [CHANNEL_NUM-1:0]               sel,                
+    output [PAYLOAD-1:0]                   payload_out   
 );
+    
+    always_comb begin
+        payload_out = '0;
+        for(int i = 0; i < CHANNEL_NUM; i++)
+        begin
+            if(sel == (1 << i))
+                payload_out = payload_in[i];
+        end
+    end
 
-
-endmodule: AHB_bus
+endmodule: AHB_mux#NUM#
