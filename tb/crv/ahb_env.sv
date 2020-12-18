3524700d685dac5b47c24bbbfb06b086928a2707
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2020-12-18 17:01:35 +0700

    Update ahb_env.sv

--- tb/crv/ahb_env.sv
+++ tb/crv/ahb_env.sv
@@ -9,65 +9,66 @@
 //================================================================================
 // Call Scoreboard from Master Driver
 //================================================================================
-class Scb_mdriver_cbs extends Ahb_mdriver_cbs;
-  Ahb_mscoreboard scb;
+class Mscb_driver_cbs extends Mas_driver_cbs;
+  Mas_scoreboard scb;
 
-  function new(Ahb_mscoreboard scb);
+  function new(Mas_scoreboard scb);
     this.scb = scb;
   endfunction: new
 
   virtual task post_tx(
-                input Ahb_mdriver drv,
-                input Slv_cell    s
+                input Mas_driver drv,
+                input Slave      s
                 );
     scb.save_expected(s);
   endtask: post_tx
 
-endclass: Scb_mdriver_cbs
+endclass: Mscb_driver_cbs
 
 //================================================================================
 // Call Scoreboard from Slave Driver
 //================================================================================
-class Scb_sdriver_cbs extends Ahb_sdriver_cbs;
-  Ahb_sscoreboard scb;
+class Sscb_driver_cbs extends Slv_driver_cbs;
+  Slv_scoreboard scb;
 
-  function new(Ahb_sscoreboard scb);
+  function new(Slv_scoreboard scb);
     this.scb = scb;
   endfunction: new
 
   virtual task post_tx(
-                input Ahb_sdriver drv,
-                input Mas_cell    m
+                input Slv_driver drv,
+                input Master     m
                 );
     scb.save_expected(m);
   endtask: post_tx
 
-endclass: Scb_sdriver_cbs
+endclass: Sscb_driver_cbs
 
 //================================================================================
 // Call Scoreboard from Master Monitor
 //================================================================================
-class Scb_mmonitor_cbs extends Ahb_mmonitor_cbs;
-  Ahb_mscoreboard scb;
+class Mscb_monitor_cbs extends Mas_monitor_cbs;
+  Slv_scoreboard scb;
 
-  function new(Ahb_mscoreboard scb);
+  function new(Slv_scoreboard scb);
     this.scb = scb;
   endfunction
 
   virtual task post_rx(
-                input Ahb_mmonitor mon,
-                input Slv_cell     s
+                input Mas_monitor mon,
+                input Slave       s
                 );
-    scb.check_actual(s, mon.portID);
+  // Check the reponse data and the Channel ID to ensure the correctness of transactions
+    scb.check_actual(s, mon.portID); 
   endtask: post_rx
 
-endclass: Scb_mmonitor_cbs
+endclass: Mscb_monitor_cbs
 
 //================================================================================
 // Call Scoreboard from Slave Monitor
 //================================================================================
-class Scb_smonitor_cbs extends Ahb_smonitor_cbs;
-  Ahb_mscoreboard scb;
+class Sscb_monitor_cbs extends Slv_monitor_cbs;
+  Slv_scoreboard scb;
 
   function new(Ahb_mscoreboard scb);
     this.scb = scb;
@@ -80,76 +81,74 @@ class Scb_smonitor_cbs extends Ahb_smonitor_cbs;
     scb.check_actual(m, mon.portID);
   endtask: post_rx
 
-endclass: Scb_smonitor_cbs
+endclass: Sscb_monitor_cbs
 
-//================================================================================
-// Call Coverage from Master Monitor
-//================================================================================
-class Cov_mmonitor_cbs extends Ahb_mmonitor_cbs
-  Ahb_mcoverage cov;
-
-  function new (Ahb_mcoverage cov);
-    this.cov = cov;
-  endfunction: new
-
-  vitual task post_rx(
-            input Ahb_mmonitor mmon,
-            input Mas_cell     m,
-            );
-  endtask: post_rx
-
-endclass: Cov_mmonitor_cbs
-
-//================================================================================
-// Call Coverage from Slave Monitor
-//================================================================================
-class Cov_smonitor_cbs extends Ahb_smonitor_cbs
-  Ahb_scoverage cov;
-
-  function new (Ahb_scoverage cov);
-    this.cov = cov;
-  endfunction: new
-
-  vitual task post_rx(
-            input Ahb_mmonitor smon,
-            input Slv_cell     s,
-            );
-  endtask: post_rx
-
-endclass: Cov_smonitor_cbs
+//cov//================================================================================
+//cov// Call Coverage from Master Monitor
+//cov//================================================================================
+//covclass Cov_mmonitor_cbs extends Ahb_mmonitor_cbs
+//cov  Ahb_mcoverage cov;
+//cov
+//cov  function new (Ahb_mcoverage cov);
+//cov    this.cov = cov;
+//cov  endfunction: new
+//cov
+//cov  vitual task post_rx(
+//cov            input Ahb_mmonitor mmon,
+//cov            input Mas_cell     m,
+//cov            );
+//cov  endtask: post_rx
+//cov
+//covendclass: Cov_mmonitor_cbs
+//cov
+//cov//================================================================================
+//cov// Call Coverage from Slave Monitor
+//cov//================================================================================
+//covclass Cov_smonitor_cbs extends Ahb_smonitor_cbs
+//cov  Ahb_scoverage cov;
+//cov
+//cov  function new (Ahb_scoverage cov);
+//cov    this.cov = cov;
+//cov  endfunction: new
+//cov
+//cov  vitual task post_rx(
+//cov            input Ahb_mmonitor smon,
+//cov            input Slv_cell     s,
+//cov            );
+//cov  endtask: post_rx
+//cov
+//covendclass: Cov_smonitor_cbs
 
 //================================================================================
 // Environment
 //================================================================================
 
-class Ahb_environment;
-  Mas_generator mgen[];
-  mailbox       mgen2drv[];
-  event         mdrv2gen[];
-  Mas_driver    mdrv[];
-  //Ahb_mconfig     mcfg;
-  Ahb_mmonitor    mmon[];
-  Ahb_mscoreboard mscb[];
+class Environment;
+  Mas_generator  mgen[];
+  mailbox        mgen2drv[];   
+  event          mdrv2gen[];
+  Mas_driver     mdrv[];
+  Mas_monitor    mmon[];
+  Mas_scoreboard mscb[];
 
-  Slv_genertor  sgen[];
-  mailbox       sgen2drv[];
-  event         sdrv2gen[];
-  Slv_driver    sdrv[];
-  //Ahb_sconfig     scfg;
-  Ahb_smonitor    smon[];
-  Ahb_sscoreboard sscb[];
+  Slv_genertor   sgen[];
+  mailbox        sgen2drv[];
+  event          sdrv2gen[];
+  Slv_driver     sdrv[];
+  Slv_monitor    smon[];
+  Slv_scoreboard sscb[];
   
-  Ahb_config     cfg[];
-  Ahb_coverage   cov[];
+  Config         cfg[];
+//cvr  Coverage   cov[];
 
-  vmas_itf    masi[];
-  vslv_itf    slvi[];  
+  vmas_itf       mas[];
+  vslv_itf       slv[];  
   
   int masnum, slvnum;
 
   extern function new(
-                input vmas_itf masi[],
-                input vslv_itf slvi[],
+                input vmas_itf mas[],
+                input vslv_itf slv[],
                 input int masnum, slvnum
                 );
   extern virtual function void gen_cfg();
@@ -160,17 +159,19 @@ class Ahb_environment;
 endclass: Ahb_env
 
 //================================================================================
+// Construct the environment instance
+//================================================================================
 
-function Ahb_environment::new(
-                input vmas_itf masi[],
-                input vslv_itf slvi[],
+function Environment::new(
+                input vmas_itf mas[],
+                input vslv_itf slv[],
                 input int masnum, slvnum
                 );
-  this.masi = new[masi.size()];
-  foreach (masi[i])
-    this.masi[i] = masi[i];
-  foreach (slvi[i])
-    this.slvi[i] = slvi[i];
+  this.mas = new[mas.size()];
+  foreach (mas[i])
+    this.mas[i] = mas[i];
+  foreach (slv[i])
+    this.slv[i] = slv[i];
   this.masnum = masnum;
   this.slvnum = slvnum;
  
@@ -185,12 +186,22 @@ function Ahb_environment::new(
 
 endfunction: new
 
-function void Ahb_environment::gen_cfg();
+//================================================================================
+// Randomize the environment
+//================================================================================
+
+function void Environment::gen_cfg();
   assert(cfg.randomize());
   cfg.display();
 endfunction: gen_cfg
 
-function void Ahb_environment::build();
+//================================================================================
+// Build the environment objects
+// The objects are built for every channels
+// Only mas_in_use channels can transfer data
+//================================================================================
+
+function void Environment::build();
   mgen = new[masnum];
   mdrv = new[masnum];
   mmon = new[masnum];
@@ -208,62 +219,70 @@ function void Ahb_environment::build();
   mcov = new();
   scov = new();
 
+  //Connect DUT with Drivers, Drivers with Generators
   foreach(mgen[i]) begin
     mgen2drv[i] = new();
-    mgen[i] = new(mgen2drv[i], mdrv2gen[i], cfg.master[i], i);
-    mdrv[i] = new(mgen2drv[i], mdrv2gen[i], masi[i], i);
+    mgen[i] = new(mgen2drv[i], mdrv2gen[i], cfg.mas_in_use[i], i); 
+    mdrv[i] = new(mgen2drv[i], mdrv2gen[i], mas[i], i);            
   end 
 
   foreach(sgen[i]) begin
     sgen2drv[i] = new();
-    sgen[i] = new(sgen2drv[i], mdrv2gen[i], cfg.slave[i], i);
-    sdrv[i] = new(mgen2drv[i], mdrv2gen[i], slvi[i], i);
+    sgen[i] = new(sgen2drv[i], mdrv2gen[i], i);
+    sdrv[i] = new(mgen2drv[i], mdrv2gen[i], slv[i], i);
   end
 
+  //Connect DUT with Monitors
   foreach(mmon[i])
-    mmon[i] = new(masi[i], i);
+    mmon[i] = new(mas[i], i);
 
   foreach(smon[i])
-    smon[i] = new(slvi[i], i);
+    smon[i] = new(slv[i], i);
    
-  // connect scoreboard with callbacks	
+  //Connect scoreboard with callbacks	
   begin
-    Ahb_mdriver_cbs smdc  = new(mscb);
-    Ahb_mmonitor_cbs smmc = new(mscb);
+    Mscb_driver_cbs  msdc = new(mscb); // Add Scoreboard to every Drivers
     foreach (mdrv[i]) 
-      mdrv[i].cbsq.push_back(smdc);
+      mdrv[i].cbsq.push_back(msdc);
+   
+    Mscb_monitor_cbs msmc = new(mscb); // Add Scoreboard to every Monitors
     foreach (mmon[i])
-      mmon[i].cbsq.push_back(smmc);
+      mmon[i].cbsq.push_back(msmc);
   end
  
   begin
-    Ahb_sdriver_cbs  ssdc  = new(sscb);
-    Ahb_smonitor_cbs ssmc = new(sscb);
+    Sscb_driver_cbs  ssdc = new(sscb);
     foreach (sdrv[i]) 
       sdrv[i].cbsq.push_back(ssdc);
+    
+    Sscb_monitor_cbs ssmc = new(sscb);
     foreach (mmon[i])
       mmon[i].cbsq.push_back(ssmc);
   end
 
-  // connect coverage wth callbacks
-  begin
-    Cov_mmonitor_cbs mc = new(mcov);    
-    foreach (mnon[i]) mmon[i].cbsq.push_back(mc);
-  end
-  
-  begin  
-    Cov_smonitor_cbs sc = new(scov);
-    foreach (smon[i]) smon[i].sbsq.push_back(sc);
-  end
+//cov  // connect coverage wth callbacks
+//cov  begin
+//cov    Cov_mmonitor_cbs mc = new(mcov);    
+//cov    foreach (mnon[i]) mmon[i].cbsq.push_back(mc);
+//cov  end
+//cov  
+//cov  begin  
+//cov    Cov_smonitor_cbs sc = new(scov);
+//cov    foreach (smon[i]) smon[i].sbsq.push_back(sc);
+//cov  end
 
 endfunction: build
 
 //================================================================================
+// Start the transactors (generators, drivers, monitors) in the environment
+// Channels that are not in use don't get started
+//================================================================================
 
-task Ahb_environment::run();
+task Environment::run();
   int running;
   running = masnum;  
 
+  // Start in_use Master channels
   foreach(mgen[i]) begin
     int j=i;
     fork
@@ -273,8 +292,10 @@ task Ahb_environment::run();
         mdrv[j].run();
       end   
     join_none
+    running--;    
   end   
-  
+ 
+  // Start all Slave channels 
   foreach(smon[i]) begin
     int j=i;
     fork
@@ -282,7 +303,7 @@ task Ahb_environment::run();
     join_none
   end
 
-  foreach(mgen[i]) begin
+  foreach(sgen[i]) begin
     int j=i;
     fork
       sgen[j].run();
@@ -301,8 +322,8 @@ task Ahb_environment::run();
     wait(running == 0);
 
     begin
-      repeat(1000000) @masi[0].master_cb);
-        $display("%t: ERRORRR: Timeout while waiting for master generators to finish", $time);
+      repeat(1000000) @mas[0].master_cb);
+        $display("%t: ERRORRR: Timeout while waiting for master transactors", $time);
         cfg.n_errors++;
     end  
   join_any
@@ -310,7 +331,7 @@ task Ahb_environment::run();
 
 endtask: run
 
-function void Ahb_environment::wrap_up();
+function void Environment::wrap_up();
   $display("%t: End of simulation, %d error%s",  
     $time, cfg.n_errors, cfg.n_errors==1 ? "" : "s");
     
