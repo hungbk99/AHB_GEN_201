167bb14a94dbadb0499e4776207cb679e75376de
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2020-12-18 17:01:57 +0700

    Update ahb_generator.sv

--- tb/crv/ahb_generator.sv
+++ tb/crv/ahb_generator.sv
@@ -1,57 +1,78 @@
 //////////////////////////////////////////////////////////////////////////////////
-// File Name: 		ahb_monitor.sv
+// File Name: 		ahb_generator.sv
 // Project Name:	AHB_Gen
 // Email:         quanghungbk1999@gmail.com
 // Version    Date      Author      Description
 // v0.0       2/10/2020 Quang Hung  First Creation
 //////////////////////////////////////////////////////////////////////////////////
 
-typedef class Ahb_mmonitor;
-//--------------------------------------------------------------------------------
- 
-class Ahb_mmonitor_cbs;
-  virtual task pre_tx( input Ahb_monitor mmon,
-                       input Slave       s);
-  endtask: pre_tx
-
-  virtual task post_tx( input Ahb_monitor mmon,
-                        input Slave      s);
-  endtask: post_tx
-endclass: Ahb_mmonitor_cbs
 
 //--------------------------------------------------------------------------------
- 
-class Ahb_mmonitor;
-  vmas_itf    mas;
-  Ahb_mmonitor_cbs cbsq[$];
-  int         portID;
-
-  extern function new( input vmas_itf mas,
-                       input int      portID);  
-
-  extern task run();
-  extern task receive ();
-
-endclass: Ahb_mmonitor
-
 //--------------------------------------------------------------------------------
 
-function Ahb_mmonitor::new(
-                       input vmas_itf mas, 
-                       input int      portID); 
-  this.mas = mas;
-  this.portID = portID;
-endfunction
+class Mas_generator;
+  Master  blueprint;
+  mailbox mas_gen2drv;
+  event   mas_drv2gen;
+  int     ncells; //numbers of gen
+  int     portID; 
+
+  function new(
+    input mailbox mas_gen2drv,
+    input event mas_drv2gen,
+    input int ncells,
+    input int portID
+  );
+    this.mas_gen2drv = mas_gen2drv;
+    this.mas_drv2gen = mas_drv2gen;
+    this.ncells = ncells;
+    this.portID = portID;
+    blueprint = new();
+  endfunction
+
+  task run();
+    Master m;
+    repeat (ncells) begin
+      assert(blueprint.randomize());
+      $cast(m, blueprint.copy()); // m is not a handle of blueprint
+      mas_gen2drv.put(m); // wait until driver receive the data
+      @mas_drv2gen
+    end
+  endtask
+
+endclass: mas_generator
 
 //--------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
 
-task Ahb_mmonitor::run();
-  Slave s;
+class Slv_generator;
+  Slave   blueprint;
+  mailbox slv__gen2drv;
+  event   slv_drv2gen;
+  int     ncells;
+  int     portID;
 
-  forever begin
-    receive(s);
-    foreach(cbsq[i])
-      cbsq[i].post_tx(this, s)
-  end   
-  
-endtask: run 
+  function new(
+    input mailbox slv_gen2drv,
+    input event   slv_drv2gen,
+    input int     ncells,
+    input int     portID
+  );
+    this.slv_gen2drv = slv_gen2drv;
+    this.slv_drv2gen = slv_drv2gen;
+    this.ncells = ncells;
+    this.portID = portID;
+    blueprint = new();
+  endfunction
+
+  task run();
+    Slave s;
+    repeat (ncells) begin
+      assert(blueprint.randomize());
+      $cast(s, blueprint.copy());
+      slv_gen2drv.put(s);
+      @slv_drv2gen
+    end
+  endtask
+
+endclass: Slv_generator
