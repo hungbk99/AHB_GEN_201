4a09d5f7c14b3ef8cef12ed51b3499337b9e98ae
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2020-12-24 16:53:04 +0700

    Update ahb_generator.sv

--- tb/crv/ahb_generator.sv
+++ tb/crv/ahb_generator.sv
@@ -36,11 +36,11 @@ class Mas_generator;
       assert(blueprint.randomize());
       $cast(m, blueprint.copy()); // m is not a handle of blueprint
       mas_gen2drv.put(m); // wait until driver receive the data
-      @mas_drv2gen
+      @mas_drv2gen;
     end
   endtask
 
-endclass: mas_generator
+endclass: Mas_generator
 
 //--------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------
@@ -71,7 +71,7 @@ class Slv_generator;
       assert(blueprint.randomize());
       $cast(s, blueprint.copy());
       slv_gen2drv.put(s);
-      @slv_drv2gen
+      @slv_drv2gen;
     end
   endtask
 
