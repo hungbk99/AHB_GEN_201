154b507e5ec9bd604e29fb399b4741ee226f4a83
Hung Quang <54548817+hungbk99@users.noreply.github.com>
2021-01-12 16:01:37 +0700

    Update config.sv

--- tb/crv/config.sv
+++ tb/crv/config.sv
@@ -2,8 +2,10 @@
  * File Name: 		config.sv
  * Project Name:	AHB_Gen
  * Email:         quanghungbk1999@gmail.com
- * Version    Date      Author      Description
- * v0.0       2/10/2020 Quang Hung  First Creation
+ * Version    Date       Author      Description
+ * v0.0       02/10/2020 Quang Hung  First Creation
+ *            12/01/2021 Quang Hung  Add support for decode error
+ *            12/01.2021 Quang Hung  Config maximum cells per masters
  *********************************************************************************/
 
 `define PRIORBIT 2
@@ -13,7 +15,7 @@ class Config;
                            slvnum;                //Slave num
   rand bit                 mas_in_use[];    //Master enable --- which master can send data?    
   rand bit [1:0]           n_cells_mas[];   //Number of cells to transmit-each master
-  bit      [1:0]           n_cells = 3;
+  bit      [7:0]           n_cells = 3;
   //Hung mod 2_1_2020 rand bit [`PRIORBIT-1:0] prior[];
   rand bit [`PRIORBIT-1:0] prior[];
  
@@ -29,27 +31,29 @@ class Config;
     {
       solve mas_in_use[i] before n_cells_mas[i];
       if(mas_in_use[i])
-        n_cells_mas[i] inside {[1:n_cells]};
+        n_cells_mas[i] inside {[2:n_cells]};//Hung_mod_12_1_2021
       else
         n_cells_mas[i] == 0;
     }
   }
 
-  extern function new(input bit [7:0] masnum, slvnum);
+  extern function new(input bit [7:0] masnum, slvnum, n_cells);
   extern virtual function void display(input string prefix="");
 
 endclass: Config
 
-function Config::new(input bit [7:0] masnum, slvnum);
+function Config::new(input bit [7:0] masnum, slvnum, n_cells);//Hung_mod_12_1_2021
   this.masnum = masnum;
   this.prior = new[masnum];
   mas_in_use = new[masnum];
   this.slvnum = slvnum;
   n_cells_mas = new[masnum];
+  this.n_cells = n_cells; //Hung_mod_12_1_2021
 endfunction: new
 
 function void Config::display(input string prefix="");
-  $display("%s:Config: %0d masters, %0d slaves", prefix, masnum, slvnum);
+  $display("=====================================================================================");
+  $display("%s:[INITIAL CONFIG]: %0d masters, %0d slaves", prefix, masnum, slvnum);
   foreach (mas_in_use[i])
   begin
     if(mas_in_use[i])
@@ -57,5 +61,6 @@ function void Config::display(input string prefix="");
     $display(); 
   end  
   $display(); 
+  $display("=====================================================================================");
 endfunction: display
                   
